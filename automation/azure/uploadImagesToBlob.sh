c2e03fb32a6eb588a8f291e3e605fcd33bbb4825
romeelkhan <romeel_k@yahoo.co.uk>
2020-06-10 18:42:05 +0100

    Updated blob shell script.

--- automation/azure/uploadImagesToBlob.sh
+++ automation/azure/uploadImagesToBlob.sh
@@ -7,3 +7,6 @@ else
     echo "About to create $STORAGEACCOUNT"
     az storage account create -n $STORAGEACCOUNT -g movieapp-rg -l westus --sku Standard_LRS
 fi
+az storage container create -account-name $STORAGEACCOUNT -name movieimages
+ACCOUNTKEY=$(az storage account keys list --account-name moviestoragerom --query [0].value)
+az storage blob upload-batch -d movieimages -s ../../src/web/movieapp/static/images/ --account-key $ACCOUNTKEY --account-name $STORAGEACCOUNT
