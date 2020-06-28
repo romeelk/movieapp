cca560eb3738982cec3586ec3ec4c7ac8c7cecad
romeelkhan <romeel_k@yahoo.co.uk>
2020-06-28 23:07:10 +0100

    Added view movie page.

--- src/web/movieapp/__init__.py
+++ src/web/movieapp/__init__.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 
 def init_app():
@@ -20,16 +21,32 @@ def init_app():
     def unhandled_exception(error):
         return render_template('unhandled.html'), 500
 
-    @app.route('/Movies')
-    def movies():
+    @app.route('/movies')
+    def get_movies():
         try:
             app.logger.info(app.config.get("MOVIEAPIURL"))
-            response = requests.get(app.config.get("MOVIEAPIURL"))
+            getmovies = app.config.get("MOVIEAPIURL") + "/movies"
+            response = requests.get(getmovies)
             movies = json.loads(response.text)
             return render_template('movies.html', movies=movies['movies'])
         except Exception:
             return render_template('unhandled.html'), 500
 
+    @app.route('/movie')
+    def movie():
+        try:
+            app.logger.info(app.config.get("MOVIEAPIURL"))
+            title = request.args.get('title')
+            print(title)
+            getmovieurl = app.config.get("MOVIEAPIURL") + "/movie/" + title
+        
+            response = requests.get(getmovieurl)
+            movie = json.loads(response.text)
+            print(response.text)
+            return render_template('movie.html', movie=movie)
+        except Exception:
+            return render_template('unhandled.html'), 500
+          
     @app.route('/')
     def home():
         return render_template('welcome.html')  # render a template
